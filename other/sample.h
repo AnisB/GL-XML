2f05ce2d6c3e3cf1d7b3b9b44aba08164867c0ca
Anis <Benyoub.anis@gmail.com>
2013-03-15 08:14:25 +0100

    Basic stuff

---
+++ other/sample.h
@@ -0,0 +1,82 @@
+/**
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+**/
+
+#pragma once
+
+/**
+* @file *nom de fichier*
+* @author *nom auteur*
+* INSTITUTION
+*
+* @date *date*
+*
+* Header file for module *nom de fichier.cpp*
+*
+*/
+
+#if defined(DGtalNode_RECURSES)
+#error Recursive header files inclusion detected in DGtalNode.h
+#else // defined(DGtalNode_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define DGtalNode_RECURSES
+
+#if !defined *nom de fichier*
+/** Prevents repeated inclusion of headers. */
+#define *nom de fichier*
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+
+//////////////////////////////////////////////////////////////////////////////
+
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  // class *nom de fichier*
+  /**
+* Description of class *nom de fichier* <p>
+*/
+
+  class Representation;
+
+
+  class DGtalNode
+  {
+    // ----------------------- Standard services ------------------------------
+
+  public:
+    /**
+* Definition de la classe
+* @param *nom parametre* *description parametre*
+*/
+
+   // ------------------------- Protected Datas ------------------------------
+
+  protected:
+    // ------------------------- Private Datas --------------------------------
+
+  private:
+
+    //--------------------------- Protected attributes---------------------------
+
+  protected:
+
+
+// 
+///////////////////////////////////////////////////////////////////////////////
+
+
+#endif

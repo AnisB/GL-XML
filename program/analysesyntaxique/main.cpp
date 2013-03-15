6daab4fe576dec10679a2922fb3c1a5078a019f3
Jeremy <jerem.gaillard@insa-lyon.fr>
2013-03-15 11:34:54 +0100

    parsing dtd and xml

--- program/analysesyntaxique/main.cpp
+++ program/analysesyntaxique/main.cpp
@@ -6,16 +6,34 @@ using namespace std;
 
 extern FILE * xmlin;
 int xmlparse(string **);
+extern FILE * dtdin;
+int dtdparse();
 
 int main(int argc, char ** argv)
 {
 	FILE * file;
-	file = fopen(argv[0], "r");
+	file = fopen(argv[1], "r");
 	if(!file) {
 		return -1;
 	}
+	cout << "Parsing XML file : " << argv[1] << endl;
 	string * nomdtd;
 	xmlin = file;
 	xmlparse(&nomdtd);
+	fclose(file);
+	
+	cout << "DTD file : " << *nomdtd << endl;
+	FILE * dtdFile;
+	dtdFile = fopen(nomdtd->data(), "r");
+	if(!dtdFile) {
+		return -1;
+	}
+	cout << "Parsing DTD file : " << *nomdtd << endl;
+	dtdin = dtdFile;
+	dtdparse();
+	fclose(dtdFile);
+	
+	
+	
 	return 0;
 } 

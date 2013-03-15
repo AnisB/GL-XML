92149eb692ee09dabd6012b649953156b5a475ad
Weillaert Florent <fweillaert@if219-07.insa-lyon.fr>
2013-03-15 09:06:19 +0100

    attribut et cdata ajouté dans la dtd

---
+++ src/dtd/attribut.h
@@ -0,0 +1,21 @@
+#include <list>
+#include "cdata.h"
+#include "uniqueelement.h"
+
+class Attribut
+{
+public:
+	Attribut(UniqueElement* element, std::list<CData*>* datas);
+	virtual ~Attribut();
+
+	void setElement(UniqueElement* element);
+	void setData(std::list<CData*>* datas);
+	void addData(CData* datas);
+	
+	std::list<CData*> * getData();
+	UniqueElement * getElement();
+
+protected:
+	std::list<CData*>* m_datas;
+	UniqueElement* m_element;
+};

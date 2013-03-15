476c04fad740cc0653cc6e0aef0ceb8d04b2b439
mcaffart <mcaffart@gmail.com>
2013-03-15 08:31:21 +0100

    filling attribute file

--- src/xml/attribut.h
+++ src/xml/attribut.h
@@ -1,1 +1,14 @@
- 
+#ifndef ATTRIBUT_H
+#define ATTRIBUT_H
+#include <iostream>
+using namespace std;
+
+class Attribut {
+	string name;
+	string value;	
+
+  public:
+	Attribut(string _name, string _value);
+	~Attribut();
+  };
+#endif

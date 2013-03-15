7eb241d7c40f1eb97822bf7c666acd4d17f79303
Anis <Benyoub.anis@gmail.com>
2013-03-15 08:32:27 +0100

    Addin DTD files

---
+++ src/dtd/content.cpp
@@ -0,0 +1,66 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file Content.cpp
+ * @author Anis Benyoub (\c benyoub.anis@gmail.com )
+ *
+ * @date date
+ *
+ * Source file for module Content
+ *
+ */
+
+
+//////////////////////////////////////////////////////////////////////////////
+//Includes
+ #include "content.h"
+//////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// IMPLEMENTATION of inline methods.
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// ----------------------- Standard services ------------------------------
+
+
+
+/**
+ * Constructor
+ */
+Content::Content ( )
+{
+}
+
+/**
+ * Destructor.
+ */
+Content::~Content( )
+{
+  //Nothing to do
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Interface - public :
+
+///////////////////////////////////////////////////////////////////////////////
+// Implementation of inline functimyCameraons //
+
+
+
+// //
+///////////////////////////////////////////////////////////////////////////////
